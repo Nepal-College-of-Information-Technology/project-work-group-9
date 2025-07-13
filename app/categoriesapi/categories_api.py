69d6ae6a0db3d7b42e9c4ba3b03f703460e11f81
Nishan giri <girinishan81@gmail.com>
2025-07-13 20:34:02 +0545

    post category

--- app/categoriesapi/categories_api.py
+++ app/categoriesapi/categories_api.py
@@ -12,3 +12,13 @@ router5 = APIRouter()
 def get_all_categories():
     categories = categories_table.all()
     return categories, 200
+
+
+@router5.post('/categories', status_code=201)
+def create_category(category: Categories):
+    existing = categories_table.get(Query().id == category.id)
+    if existing:
+        raise HTTPException(status_code=400, detail="Category ID already exists")
+    categories_dict=json.loads(category.model_dump_json())
+    categories_id=categories_table.insert(categories_dict)
+    return {**categories_dict}
